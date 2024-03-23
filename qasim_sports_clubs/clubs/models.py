5fdbdd02787e2d94dbe232645d6b492b6dd6352a
ragaln <159661174+ragaln@users.noreply.github.com>
2024-03-23 22:59:57 +0000

    create models for sport and club

--- qasim_sports_clubs/clubs/models.py
+++ qasim_sports_clubs/clubs/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Sport(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Club(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=200)
+    sport = models.ForeignKey(Sport, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} ({self.sport.name})"
