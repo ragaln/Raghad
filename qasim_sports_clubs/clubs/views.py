eabf4671cb3165f2ed264d8e3f199d9ff9455cf3
ragaln <159661174+ragaln@users.noreply.github.com>
2024-03-27 23:10:32 +0000

    use model to get and filter data

--- qasim_sports_clubs/clubs/views.py
+++ qasim_sports_clubs/clubs/views.py
@@ -1,57 +1,40 @@
 from django.shortcuts import redirect, render
-
-# Create your views here.
-# it is about qasim sports clubs
-
-def __getClubs():
-    return [
-        {'id': 1, 'name': 'Al-Qasim Football Club', 'sport': 'Football', 'location': 'Qasim Stadium', 'image': 'images/football.jpg'},
-        {'id': 2, 'name': 'Al-Qasim Tennis Club', 'sport': 'Tennis', 'location': 'Qasim Tennis Courts', 'image': 'images/tennis.jpg'},
-        {'id': 3, 'name': 'Al-Qasim Basketball Club', 'sport': 'Basketball', 'location': 'Qasim Sports Arena', 'image': 'images/basketball.jpg'}
-    ]
-
-
-
-def index(request):
-    # This view returns the index page for Qasim Sports Clubs
-	return render(request, 'clubsmodule/index.html')
+from .models import Club, Sport
 
 def clubs(request):
-    # This view returns the clubs list page for Qasim Sports Clubs
-    all_clubs = __getClubs()
+    all_clubs = Club.objects.select_related('sport').all()
     context = {'clubs': all_clubs}
     return render(request, 'clubsmodule/clubList.html', context)
-def contactus(request):
-    # This view returns the contact us page 
-    return render(request, 'clubsmodule/contact.html')
 
 def club(request, cId):
-    # This view returns the details of a specific club for Qasim Sports Clubs
-    
-    club1 = {'id':12344321, 'name':'Football Club', 'location':'Stadium A'}
-    club2 = {'id':56788765, 'name':'Tennis Club', 'location':'Court B'}
-    
-    targetClub = None
-    if club1['id'] == cId: targetClub = club1
-    if club2['id'] == cId: targetClub = club2
-    
-    if targetClub == None: return redirect('/clubs')
-    
-    context = {'club':targetClub} # 'club' is the variable name accessible by the template
+    try:
+        targetClub = Club.objects.select_related('sport').get(id=cId)
+    except Club.DoesNotExist:
+        return redirect('/clubs')
+    context = {'club': targetClub}
     return render(request, 'clubsmodule/club.html', context)
+
+def index(request):
+    return render(request, 'clubsmodule/index.html')
+
+def contactus(request):
+    return render(request, 'clubsmodule/contact.html')
 
 def filter(request):
     return render(request, 'clubsmodule/filter.html')
 
 def filter_clubs(request):
-    clubs = __getClubs()
     name_query = request.GET.get('name', '')
     sport_query = request.GET.get('sport', '')
 
-    if name_query:
-        clubs = [club for club in clubs if name_query.lower() in club['name'].lower()]
-    if sport_query:
-        clubs = [club for club in clubs if club['sport'] == sport_query]
+    if name_query and sport_query:
+        clubs = Club.objects.filter(name__icontains=name_query, sport__name=sport_query)
+    elif name_query:
+        clubs = Club.objects.filter(name__icontains=name_query)
+    elif sport_query:
+        clubs = Club.objects.filter(sport__name=sport_query)
+    else:
+        clubs = Club.objects.all()
 
     context = {'clubs': clubs}
     return render(request, 'clubsmodule/clubList.html', context)
