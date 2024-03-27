ef68a66184ab6509513a1172e35ec9b584c749b6
ragaln <159661174+ragaln@users.noreply.github.com>
2024-03-27 22:52:11 +0000

    migrate model to dataase

--- qasim_sports_clubs/clubs/models.py
+++ qasim_sports_clubs/clubs/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 
 class Sport(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
 
 class Club(models.Model):
-    name = models.CharField(max_length=100)
-    location = models.CharField(max_length=200)
+    name = models.CharField(max_length=50)
+    location = models.CharField(max_length=50)
     sport = models.ForeignKey(Sport, on_delete=models.CASCADE)
 
     def __str__(self):
